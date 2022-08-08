ebabbcdff6017a65c174900bca553e13a6b9a21c
Gustavo <anteiku04@hotmail.com>
2022-08-08 10:44:27 -0300

    api rest first commit

---
+++ src/controllers/UserController.js
@@ -0,0 +1,70 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const newUser = await User.create(req.body);
+      const { id, name, email } = newUser;
+      return res.json({ id, name, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll({ attributes: ['id', 'name', 'email'] });
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User doesn't exists."],
+        });
+      }
+      const updatedData = await user.update(req.body);
+      const { id, name, email } = updatedData;
+      return res.json({ id, name, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User doesn't exists."],
+        });
+      }
+      await user.destroy();
+      return res.json(`${user.name} was deleted sucessfully.`);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+}
+
+export default new UserController();
